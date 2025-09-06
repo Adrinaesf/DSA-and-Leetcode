326b0710c37367bf1c710a405e92a6a1209aedc8
Adrina <24esfaad@ntcs.on.ca>
2025-09-06 11:47:01 -0400

    Add DynamicArray implementation

--- mainStack.py
+++ mainStack.py
@@ -2,10 +2,19 @@
 ## Using Stacks Class
 ##
 from stackClass import Stack
+from dynamicArray import DynamicArray
 
 my_stack = Stack()
 my_stack.extend([1, 2, 3])
+print("Stack: ")
 print(my_stack)
+
+my_dynArr = DynamicArray(2)
+my_dynArr.pushBack(2)
+my_dynArr.pushBack(3)
+my_dynArr.pushBack(4)
+print("Dynamic Array:")
+print(my_dynArr)
 
 
 
