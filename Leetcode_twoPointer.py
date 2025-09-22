291ec9d79b1e95b7a849ba4de2355c8934a6c7a9
Adrina <adrinacad.esf@gmail.com>
2025-09-22 15:57:15 -0400

    Added the solution to the max area question

--- Leetcode_twoPointer.py
+++ Leetcode_twoPointer.py
@@ -29,3 +29,18 @@ class Solution:
                 R -= 1
             else: 
                 L += 1
+
+  def maxArea(self, heights: List[int]) -> int:
+        res = 0
+        L, R = 0, len(heights) - 1
+
+
+        while L < R:
+            area = (R - L) * min(heights[L], heights[R])
+            res = max(res, area)
+            if heights[L] <= heights[R]:
+                L += 1
+            else: 
+                R -=1
+
+        return res
